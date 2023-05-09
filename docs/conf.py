f4f41f268ae40a054d2df9b0553cba909ac1a890
jay gray <jaygray0919@gmail.com>
2023-05-09 08:42:17 -0500

    improved bifurcation

--- docs/conf.py
+++ docs/conf.py
@@ -168,7 +168,9 @@ html_logo = '_images/onto-logo-text-blue-w325-h60.svg'
 
 # The name of an image file (within the static path) to use as favicon of the docs.
 # This file should be a Windows icon file (.ico) being 16x16 or 32x32 pixels large.
-html_favicon = "onto-shortcut-w252-h252-color-ffffff-bgnd-1f64ff.svg"
+
+# need version white
+#html_favicon = "onto-shortcut-w252-h252-color-ffffff-bgnd-1f64ff.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here, relative to this directory.
 # They are copied after the built-in static files, so a file named "default.css" will overwrite the built-in "default.css".
